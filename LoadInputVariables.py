7ee5c9e3303d69c40792102a3b9dc9f4bd577cc1
Paahuni Khandelwal <paahuni@lattice-176.cs.colostate.edu>
2020-12-11 01:05:50 -0700

    TO DO

--- LoadInputVariables.py
+++ LoadInputVariables.py
@@ -10,14 +10,18 @@ import stippy
 import torch
 from time import strptime, mktime
 import ee
+import pprint
+
 service_account = 'fineet-paahuni@appspot.gserviceaccount.com'
-credentials = ee.ServiceAccountCredentials(service_account, 'satelliteimagery-275021-d84dc86097ba.json')
-ee.Initialize(credentials)
+credentials = ee.ServiceAccountCredentials(service_account, '../satelliteimagery-275021-d84dc86097ba.json')
+# ee.Authenticate()
+# ee.Initialize(credentials)
 import calcs
+
 
 class generate_input_data():
     def __init__(self, pix_cover=0.1, cloud_cov=0.1, geoh=None,
-                 dataset=None, startT=None, endT= None,
+                 dataset=None, startT=None, endT=None,
                  impute_high_resolution=False, albums='colorado', is_output_image=True):
 
         self.startT = startT
@@ -29,14 +33,14 @@ class generate_input_data():
         self.dataset = dataset
         self.albums = albums
         self.impute_high_resolution = impute_high_resolution
-        self.target_image_size = (10,10)
-        self.is_output_image = is_output_image     # point or image
-        self.platform=''
+        self.target_image_size = (10, 10)
+        self.is_output_image = is_output_image  # point or image
+        self.platform = ''
         if self.dataset == 'modis-weekly':
             self.platform = 'MOD11A2'
         elif self.dataset == 'landsat':
             self.platform = 'Landsat8C1L1'
-        elif self.dataset== 'viirs':
+        elif self.dataset == 'viirs':
             self.platform = 'VNP21v001'
         else:
             self.platform = 'MOD11A1'
@@ -44,16 +48,18 @@ class generate_input_data():
         if geoh is None:
             stip_iter = stippy.list_node_images(self.host_addr, platform=self.platform, album='colorado',
                                                 min_pixel_coverage=1.0, source='raw', max_cloud_coverage=self.cloud_cov,
-                                                start_timestamp=startT, end_timestamp=endT, recurse= True
+                                                start_timestamp=self.convertDateToEpoch(startT),
+                                                end_timestamp=self.convertDateToEpoch(endT), recurse=True
                                                 )
         else:
-            stip_iter = stippy.list_node_images(self.host_addr, platform=self.platform,album='colorado',
+            stip_iter = stippy.list_node_images(self.host_addr, platform=self.platform, album='colorado',
                                                 min_pixel_coverage=1.0, source='raw', max_cloud_coverage=self.cloud_cov,
-                                                start_timestamp=startT, end_timestamp=endT, geocode=geoh
+                                                start_timestamp=self.convertDateToEpoch(startT),
+                                                end_timestamp=self.convertDateToEpoch(endT), geocode=geoh
                                                 )
-        fileEndingIndex = {'MOD11A1' : 1, 'MOD11A2' : 1, 'VNP21v001' : 2, 'Landsat8C1L1' : 3}
-        self.thermalBandIndex = {'MOD11A1': [1,3], 'MOD11A2': [2,5], 'VNP21v001': [2,5], 'Landsat8C1L1': [3,1]}
-        self.datafilenames,self.timestamp = [],[]
+        fileEndingIndex = {'MOD11A1': 1, 'MOD11A2': 1, 'VNP21v001': 2, 'Landsat8C1L1': 3}
+        self.thermalBandIndex = {'MOD11A1': [1, 3], 'MOD11A2': [2, 5], 'VNP21v001': [2, 5], 'Landsat8C1L1': [3, 1]}
+        self.datafilenames, self.timestamp = [], []
         for i in stip_iter:
             paths = []
             for file in i[1].files:
@@ -63,6 +69,7 @@ class generate_input_data():
 
             self.datafilenames.append(paths)
             self.timestamp.append(datetime.utcfromtimestamp(i[1].timestamp))
+        ee.Initialize()
 
     def __len__(self):
         return len(self.datafilenames)
@@ -80,11 +87,9 @@ class generate_input_data():
         all_thermal_bands = torch.stack(images).squeeze(0)
         return all_thermal_bands
 
-
     def get_ndvi(self, inp_image):
         # Update for different dataset
         return (inp_image[:, 2] - inp_image[:, 11]) / (inp_image[:, 2] + inp_image[:, 11])
-
 
     def predict_missing_band(self):
         '''
@@ -104,7 +109,7 @@ class generate_input_data():
         return
 
     def convertDateToEpoch(self, inputDate):
-        dateF = strptime(inputDate + ' 00:00', '%d-%m-%Y %H:%M')
+        dateF = strptime(inputDate + ' 00:00', '%Y-%m-%d %H:%M')
         epochTime = mktime(dateF)
         return int(epochTime)
 
@@ -119,23 +124,20 @@ class generate_input_data():
         :return: tmax, tmin, ea, rs, uz, zw, elev, lat
         '''
         if not isDatasetDownloaded:
-            images = ee.Image(ee.ImageCollection('IDAHO_EPSCOR/GRIDMET').
-                     filterDate(self.startT, self.endT))
-
-            print(len(images))
+            images = ee.ImageCollection('IDAHO_EPSCOR/GRIDMET').filterDate(self.startT, self.endT)
+            # print("Found {} images for GRIMET data in given time frame".format(images.size()))
             # tmax, tmin = self.temp_bands()
-            # for img in images:
-            #     tmin = img.select(['tmmn']).subtract(273.15).rename(['tmin'])
-            #     tmax = img.select(['tmmx']).subtract(273.15).rename(['tmax'])
-            #     uz = img.select(['vs']).rename(['uz'])
-            #     rs = img.select(['srad']).multiply(0.0864).rename(['rs'])
-            #     ea = calcs._actual_vapor_pressure(
-            #         q=img.select(['sph']),
-            #         pair=calcs._air_pressure(elev, 'asce')).rename(['ea'])
-            #     lat_ = np.array((ee.Array(data.get("lat")).getInfo()))
-            #     long_ = np.array((ee.Array(data.get("long")).getInfo()))
+            for img in images:
+                tmin = img.select(['tmmn']).subtract(273.15).rename(['tmin'])
+                tmax = img.select(['tmmx']).subtract(273.15).rename(['tmax'])
+                uz = img.select(['vs']).rename(['uz'])
+                rs = img.select(['srad']).multiply(0.0864).rename(['rs'])
+                ea = calcs._actual_vapor_pressure(
+                    q=img.select(['sph']),
+                    pair=calcs._air_pressure(elev, 'asce')).rename(['ea'])
+                yield tmin, tmax, uz, rs, ea
 
-        #Currently supports Gridmet dataset using EE tool;
+        # Currently supports Gridmet dataset using EE tool;
         else:
             if self.geoh is None:
                 stip_iter = stippy.list_node_images(self.host_addr, platform='GRIDMET', album='colorado',
@@ -147,13 +149,13 @@ class generate_input_data():
                 stip_iter = stippy.list_node_images(self.host_addr, platform='GRIDMET', album='colorado',
                                                     min_pixel_coverage=1.0, source='raw',
                                                     max_cloud_coverage=self.cloud_cov,
-                                                    start_timestamp=self.startT, end_timestamp=self.endT, geocode=self.geoh
+                                                    start_timestamp=self.startT, end_timestamp=self.endT,
+                                                    geocode=self.geoh
                                                     )
-
 
         return
 
-if __name__ == '__main__':
-    loaded_data = generate_input_data()
-    loaded_data.input_features()
 
+if __name__ == '__main__':
+    loaded_data = generate_input_data(startT='2018-03-01', endT='2018-08-01')
+    loaded_data.input_features()
